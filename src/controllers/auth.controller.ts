aa7e64fd61f9fbe1b72992b5d4a3fd1fe19a0d4e
lizzy596 <lizmeek123@gmail.com>
2023-05-03 09:25:36 -0500

    added ts-node

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,7 +1,8 @@
-const httpStatus = require('http-status');
-const catchAsync = require('../utils/catchAsync');
-const config = require('../config/config');
-const {
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../utils/catchAsync';
+import config from '../config/config';
+import {
   authService,
   userService,
   tokenService,
@@ -9,16 +10,16 @@ const {
   cookieService,
   appleService,
   googleService,
-} = require('../services');
+} from '../services';
 
-const register = catchAsync(async (req, res) => {
+const register = catchAsync(async (req: Request, res: Response) => {
   Object.assign(req.body, { authType: 'email' });
   const user = await userService.createUser(req.body);
   const tokens = await tokenService.generateAuthTokens(user);
   res.status(httpStatus.CREATED).send({ user, tokens });
 });
 
-const loginEmail = catchAsync(async (req, res) => {
+const loginEmail = catchAsync(async (req: Request, res: Response) => {
   const { email, password } = req.body;
   const user = await authService.loginUserWithEmailAndPassword(email, password);
   const tokens = await tokenService.generateAuthTokens(user);
@@ -27,7 +28,7 @@ const loginEmail = catchAsync(async (req, res) => {
 });
 
 // loginGoogle
-const loginGoogle = catchAsync(async (req, res) => {
+const loginGoogle = catchAsync(async (req: Request, res: Response) => {
   const user = await googleService.verifyOAuthToken(req.body.token);
   const tokens = await tokenService.generateAuthTokens(user);
   cookieService.setTokenCookie(res, tokens.refresh);
@@ -35,56 +36,13 @@ const loginGoogle = catchAsync(async (req, res) => {
 });
 
 // loginApple
-const loginApple = catchAsync(async (req, res) => {
+const loginApple = catchAsync(async (req: Request, res: Response) => {
   const user = await appleService.verifyOAuthToken(req.body.token);
   const tokens = await tokenService.generateAuthTokens(user);
   cookieService.setTokenCookie(res, tokens.refresh);
   res.send({ user, tokens });
 });
 
-const logout = catchAsync(async (req, res) => {
-  await authService.logout(req.body.refreshToken);
-  cookieService.expireTokenCookie(res);
-  res.status(httpStatus.NO_CONTENT).send();
-});
 
-const refreshTokens = catchAsync(async (req, res) => {
-  const { user, tokens } = await authService.refreshAuth(req.cookies[config.jwt.refreshCookieName] || req.body.refreshToken);
-  cookieService.setTokenCookie(res, tokens.refresh);
-  res.send({ user, tokens });
-});
+ 
 
-const forgotPassword = catchAsync(async (req, res) => {
-  const resetPasswordToken = await tokenService.generateResetPasswordToken(req.body.email);
-  await emailService.sendResetPasswordEmail(req.body.email, resetPasswordToken);
-  res.status(httpStatus.NO_CONTENT).send();
-});
-
-const resetPassword = catchAsync(async (req, res) => {
-  await authService.resetPassword(req.query.token, req.body.password);
-  res.status(httpStatus.NO_CONTENT).send();
-});
-
-const sendVerificationEmail = catchAsync(async (req, res) => {
-  const verifyEmailToken = await tokenService.generateVerifyEmailToken(req.user);
-  await emailService.sendVerificationEmail(req.user.email, verifyEmailToken);
-  res.status(httpStatus.NO_CONTENT).send();
-});
-
-const verifyEmail = catchAsync(async (req, res) => {
-  await authService.verifyEmail(req.query.token);
-  res.status(httpStatus.NO_CONTENT).send();
-});
-
-module.exports = {
-  register,
-  loginEmail,
-  loginApple,
-  loginGoogle,
-  logout,
-  refreshTokens,
-  forgotPassword,
-  resetPassword,
-  sendVerificationEmail,
-  verifyEmail,
-};
