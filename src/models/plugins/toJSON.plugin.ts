d281340986679a38b8a8652ed3679f608e954bed
lizzy596 <lizmeek123@gmail.com>
2023-05-05 15:52:28 -0500

    fixed the toJSON plugin

--- src/models/plugins/toJSON.plugin.ts
+++ src/models/plugins/toJSON.plugin.ts
@@ -1,31 +1,45 @@
-import { Schema, Document } from 'mongoose';
+/* eslint-disable no-param-reassign */
+import { Document } from 'mongoose';
 
-const deleteAtPath = (obj: Record<string, any>, path: string[], index: number): void => {
+/**
+ * A mongoose schema plugin which applies the following in the toJSON transform call:
+ *  - removes __v, createdAt, updatedAt, and any path that has private: true
+ *  - replaces _id with id
+ */
+
+const deleteAtPath = (obj: any, path: any, index: number) => {
   if (index === path.length - 1) {
+    // eslint-disable-next-line no-param-reassign
     delete obj[path[index]];
     return;
   }
   deleteAtPath(obj[path[index]], path, index + 1);
 };
 
-const toJSON = (schema: Schema): void => {
-  let transform;
+const toJSON = (schema: any) => {
+  let transform: Function;
   if (schema.options.toJSON && schema.options.toJSON.transform) {
     transform = schema.options.toJSON.transform;
   }
 
+  // eslint-disable-next-line no-param-reassign
   schema.options.toJSON = Object.assign(schema.options.toJSON || {}, {
-    transform(doc: Document, ret: Record<string, any>, options: any) {
+    transform(doc: Document, ret: any, options: Record<string, any>) {
       Object.keys(schema.paths).forEach((path) => {
         if (schema.paths[path].options && schema.paths[path].options.private) {
           deleteAtPath(ret, path.split('.'), 0);
         }
       });
 
+      // eslint-disable-next-line no-param-reassign
       ret.id = ret._id.toString();
+      // eslint-disable-next-line no-param-reassign
       delete ret._id;
+      // eslint-disable-next-line no-param-reassign
       delete ret.__v;
+      // eslint-disable-next-line no-param-reassign
       delete ret.createdAt;
+      // eslint-disable-next-line no-param-reassign
       delete ret.updatedAt;
       if (transform) {
         return transform(doc, ret, options);
@@ -35,3 +49,4 @@ const toJSON = (schema: Schema): void => {
 };
 
 export default toJSON;
+
